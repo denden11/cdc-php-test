ab65b73aa0b4cb51b22aef5ca6d6939ebb0e7516
denden11 <abeniondennis11@gmail.com>
2021-07-27 13:54:24 +0800

    3

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <?php
-
- echo "test";
-//  try {
-//   $apiKey = "3_ojnmzVXjaGTCcK4MHoSjrWzeXQPFfYHRW9XvKMPEf4PzI6kliiUrY924BBtaZuBQ";
-//   $secretKey = "Plfs1pnOyevXimj3ah9Ado+MmTZMk/KBrIgSInM+sj8=";
+ use Gigya\PHP\GSRequest;
+ try {
+  $apiKey = "3_ojnmzVXjaGTCcK4MHoSjrWzeXQPFfYHRW9XvKMPEf4PzI6kliiUrY924BBtaZuBQ";
+  $secretKey = "Plfs1pnOyevXimj3ah9Ado+MmTZMk/KBrIgSInM+sj8=";
   
-//   $method = "accounts.setAccountInfo";
+  $method = "accounts.setAccountInfo";
   
-//   $request = new GSRequest($apiKey,$secretKey,$method);
-//   $request->setParam("UID","e8e80239079e4a64a5bfdb480407e7f8");
-//   $request->setParam("isActive",false);
+  $request = new GSRequest($apiKey,$secretKey,$method);
+  $request->setParam("UID","e8e80239079e4a64a5bfdb480407e7f8");
+  $request->setParam("isActive",false);
   
-//   $response = $request->send();
+  $response = $request->send();
   
-//   if($response->getErrorCode()==0)
-//       {
-//           echo "Success";
-//       }
-//   else
-//       {
-//           echo ("Uh-oh, we got the following error: " . $response->getErrorMessage());
-//           error_log($response->getLog());
-//       }
-// } catch (Exception $e) {
-//     echo 'Caught exception: ',  $e->getMessage(), "\n";
-// }
+  if($response->getErrorCode()==0)
+      {
+          echo "Success";
+      }
+  else
+      {
+          echo ("Uh-oh, we got the following error: " . $response->getErrorMessage());
+          error_log($response->getLog());
+      }
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 ?>
